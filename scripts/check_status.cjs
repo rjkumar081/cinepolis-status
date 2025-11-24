85fdd3a8cd5400b4d1ebccc76b13f7668a3d4fee
rjkumar081 <97723829+rjkumar081@users.noreply.github.com>
2025-11-24 10:56:09 +0530

    Update check_status.cjs

--- scripts/check_status.cjs
+++ scripts/check_status.cjs
@@ -13,7 +13,7 @@ const MAX_LOG = 9440;
 (async () => {
   await fs.ensureDir(LOG_FOLDER);
 
-  let status = null;
+  let status = UP;
   let responseTime = 0;
   let checkedAt = new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" });
 
